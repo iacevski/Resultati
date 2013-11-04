14d0d5991580761d988711a9251b618dcd225a44
iacevski74 <iacevski74@gmail.com>
2013-11-05 00:17:52 +0100

    KreiranjePredmeti

---
+++ Glavna/Predmeti/Predmeti.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Predmeti
+{
+    public class Predmeti
+    {
+        public Predmeti() 
+        {
+            RezultatiList = new List<Rezultati>();
+        }
+
+        public int PredmetId { get; set; }
+        [Required]
+        public string PredmetName { get; set; }
+        public string PredmetSemestar { get; set; }
+
+        public virtual ICollection<Rezultati> RezultatiList { get; set; }
+    }
+}
