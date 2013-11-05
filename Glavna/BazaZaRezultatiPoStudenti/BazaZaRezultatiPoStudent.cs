fcef30654e532c51a5f9f12bba5ad7d457149623
iacevski74 <iacevski74@gmail.com>
2013-11-05 09:43:18 +0100

    Sega4greski

---
+++ Glavna/BazaZaRezultatiPoStudenti/BazaZaRezultatiPoStudent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+using Rezultati;
+
+namespace BazaZaRezultatiPoStudenti
+{
+    public class BazaZaRezultatiPoStudent : DbContext
+    {
+        public BazaZaRezultatiPoStudent(): base() 
+        {
+        }
+
+        public DbSet<Student> Studenti { get; set; }
+        
+        public DbSet<Rezultat> ListaRezultati { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //one-to-many 
+            modelBuilder.Entity<Rezultat>().HasRequired<Student>(s => s.Student)
+            .WithMany(s => s.ListaRezultati).HasForeignKey(s => s.StudentId);
+
+
+        }
+    }
+}
